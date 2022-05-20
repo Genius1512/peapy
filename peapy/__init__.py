8b37e199ca6fadb71f4003b4406f52afa4e822a4
Genius1512 <silvan.genius@gmail.com>
2022-05-20 19:58:05 +0200

    Reinit

--- peapy/__init__.py
+++ peapy/__init__.py
@@ -2,9 +2,4 @@
 An easy-to-use Python game engine.
 """
 
-from . import config
-from . import exceptions
-from . import utils
-from .component import Component
-from .game_object import GameObject
 from .peapy import PeaPy
